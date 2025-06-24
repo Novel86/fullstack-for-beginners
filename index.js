45bf26b595f79707cfdc5681c76576a0624509ed
Roman <86733860+Novel86@users.noreply.github.com>
2025-06-24 09:57:27 +0700

    some minor update

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const PORT = 5000;
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-	res.status(200).json('Server start succesfully!!!');
+	res.status(200).json([{ status: res.statusCode }, req.query]);
+});
+app.post('/', (req, res) => {
+	res.status(200).json(req.body);
 });
 
 app.listen(PORT, () => console.log(`server start on PORT: ${PORT}`));
